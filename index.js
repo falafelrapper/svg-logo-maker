c8b387daf0c08c4ea33285bf416972e9e1887464
Zachary Roy <vgd199@gmail.com>
2024-01-23 19:26:08 -0800

    Added more to my index.js, figuring out the functions currently

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
 // Main script
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What would you like your logo to say?',
+    },
+    {
+        type: 'input',
+        message: 'What color would you like your text?',
+        name: 'textcolor',
+    },
+    {
+        type: 'checkbox',
+        message: 'What shape would you like to use?',
+        name: 'shape',
+        choices: ['Triangle', 'Square', 'Circle'],
+    },
+    {
+        type: 'input',
+        message: 'What color would you like your shape?',
+        name: 'shapecolor',
+    },
+];
+
+// inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             name: 'text',
+//             message: 'What would you like your logo to say?',
+//         },
+//         {
+//             type: 'input',
+//             message: 'What color would you like your text?',
+//             name: 'textcolor',
+//         },
+//         {
+//             type: 'checkbox',
+//             message: 'What shape would you like to use?',
+//             name: 'shape',
+//             choices: ['Triangle', 'Square', 'Circle'],
+//         },
+//         {
+//             type: 'input',
+//             message: 'What color would you like your shape?',
+//             name: 'shapecolor',
+//         },
+//     ])
+//     .then((data) => {
+
+//         fs.writeFile('shape.svg', JSON.stringify(data, null, '\t'), (err) =>
+//             err ? console.log(err) : console.log('Success!')
+//         );
+//     });
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+            const finalSVG = generateShape(answers);
+
+            fs.writeFile('./shape.SVG', finalSVG, (err) =>
+                err ? console.log(err) : console.log('Successfully created README!')
+            );
+        })
+};
+
+init();
