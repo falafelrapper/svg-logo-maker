608494648b7656ed76ed9ab4b8ffa47178910754
Zachary Roy <vgd199@gmail.com>
2024-01-23 20:16:19 -0800

    Made a placeholder README and added a switch statement to my index

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# svg-logo-maker
+# Fullstack Development Challenge 8
+
+## Welcome to the eighth Challenge of the UCLA Bootcamp!
+
+In this challenge we were supposed to start from a scratch and make an SVG logo generator! To do this, we are using node.js to start an index.js file and have it generate a .SVG file that we can use as our new logo! To start, lets look at our requirements:
+
+## User Story
+
+```md
+AS a freelance web developer
+I WANT to generate a simple logo for my projects
+SO THAT I don't have to pay a graphic designer
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I have entered input for all the prompts
+THEN an SVG file is created named `logo.svg`
+AND the output text "Generated logo.svg" is printed in the command line
+WHEN I open the `logo.svg` file in a browser
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+```
+
+## The Completed Challenge
+
+After completing the challenge, we should now have a working index.js file that will allow you to answer a series of questions now. From there, after completing these questions, you should be presented with a new .SVG file in your folder after you have downloaded this file.
+
+To view a demo of this application, please click the following link here.
+
+## Credits
+This code was completed by Zachary Roy
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Main script
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Circle = require('./lib/shapes')
+// const generateShape = require('./lib/shapes.js')
 
 const questions = [
     {
@@ -26,47 +26,29 @@ const questions = [
         name: 'shapecolor',
     },
 ];
-
-// inquirer
-//     .prompt([
-//         {
-//             type: 'input',
-//             name: 'text',
-//             message: 'What would you like your logo to say?',
-//         },
-//         {
-//             type: 'input',
-//             message: 'What color would you like your text?',
-//             name: 'textcolor',
-//         },
-//         {
-//             type: 'checkbox',
-//             message: 'What shape would you like to use?',
-//             name: 'shape',
-//             choices: ['Triangle', 'Square', 'Circle'],
-//         },
-//         {
-//             type: 'input',
-//             message: 'What color would you like your shape?',
-//             name: 'shapecolor',
-//         },
-//     ])
-//     .then((data) => {
-
-//         fs.writeFile('shape.svg', JSON.stringify(data, null, '\t'), (err) =>
-//             err ? console.log(err) : console.log('Success!')
-//         );
-//     });
+ 
 
 function init() {
     inquirer
         .prompt(questions)
         .then((answers) => {
             console.log(answers);
-            const finalSVG = generateShape(answers);
+            switch (answers.shape) {
+                case "Circle":
+                //create a circle instance with the new keyword
+                  break;
+                case "Square":
+                //create a square instance with the new keyword
+                  break;
+      
+                case "Triangle":
+                //create a triangle instance with the new keyword
+                  break;
+            }
+            // const finalSVG = generateShape(answers);
 
             fs.writeFile('./shape.SVG', finalSVG, (err) =>
-                err ? console.log(err) : console.log('Successfully created README!')
+                err ? console.log(err) : console.log('Successfully created your logo!')
             );
         })
 };
